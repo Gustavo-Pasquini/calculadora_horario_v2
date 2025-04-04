de57ca42d11a70bf141289dcdd606ee17e41ca4f
Gustavo Mazeto Pasquini <gustavopasquini2006@hotmail.com>
2025-04-04 10:04:34 -0300

    dotenv adjusts

--- src/config/firestore.ts
+++ src/config/firestore.ts
@@ -10,13 +10,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC70tn1M08W6TSHahhfTIuPBw-lUUtj-EU", //process.env.REACT_APP_API_KEY,
-  authDomain: "calculadora-horarios-v2.firebaseapp.com", //process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: "calculadora-horarios-v2", //process.env.REACT_APP_PROJECT_ID,
-  storageBucket: "calculadora-horarios-v2.firebasestorage.app", //process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: "217559594958", //process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: "1:217559594958:web:9293409f4f344a7201dd9f", //process.env.REACT_APP_APP_ID,
-  measurementId: "G-Y5XBNRSB4Z" //process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
